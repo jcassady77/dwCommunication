bf297414acd632d3d216fc0b15aa5b28c053ffe7
jcassady77 <jcassady@andrew.cmu.edu>
2021-05-21 20:06:30 -0400

    creating new struct

--- src/dw_listener/src/dw_node.cpp
+++ src/dw_listener/src/dw_node.cpp
@@ -9,6 +9,24 @@
 #include <unistd.h>
 #include <dw_listener/nodeData.h>
 
+class dw_data {
+    public:
+        string tagAddress;
+        int64 rangeNum;
+        int64 timeOfReception;
+        int64 distance;
+        int64 degrees;
+        int64 Xcoord;
+        int64 Ycoord;
+        int64 clockOffset;
+        int64 serviceData;
+        int64 Xaccel;
+        int64 Yaccel;
+        int64 Zaccel;
+
+
+}
+
 
 int main(int argc, char **argv)
 {
@@ -71,3 +89,80 @@ int main(int argc, char **argv)
     return 0;
 
 }
+
+//  CODE FROM DECAWAVE GUI
+
+/**
+* @brief motionFilter()
+*        perfrom the motion-filter on the x and y inputs
+* */
+/**
+void motionFilter(double* x, double* y, int i)
+{
+     tag_reports_t rp = _tagList.at(i);
+
+     if (rp.filterHisIdx >= FILTER_SIZE)
+     {
+         rp.filterHisIdx = static_cast<int>(fmod(rp.filterHisIdx,FILTER_SIZE));
+         rp.motionFilterReady = true;
+     }
+
+     rp.estXHis[rp.filterHisIdx] = *x;
+     rp.estYHis[rp.filterHisIdx] = *y;
+
+     rp.filterHisIdx = rp.filterHisIdx + 1;
+
+     if (_motionFilterOn)
+     {
+         if(rp.motionFilterReady)
+         {
+             double tempX[FILTER_SIZE];
+             memcpy(tempX, &rp.estXHis[0], sizeof(tempX));
+
+             double tempY[FILTER_SIZE];
+             memcpy(tempY, &rp.estYHis[0], sizeof(tempY));
+
+             r95Sort(tempX,0,FILTER_SIZE-1);
+
+             *x = (tempX[FILTER_SIZE/2] + tempX[FILTER_SIZE/2-1])/2;
+
+             r95Sort(tempY,0,FILTER_SIZE-1);
+
+             *y = (tempY[FILTER_SIZE/2] + tempY[FILTER_SIZE/2-1])/2;
+         }
+
+     }
+
+     //update the list entry
+     _tagList.replace(i, rp);
+ }
+
+/**
+* @brief r95Sort()
+*        R95 sort used by the motion filter function above
+* */
+/**
+void r95Sort (double s[], int l, int r)
+{
+    int i,j;
+    double x;
+    if(l<r)
+    {
+        i = l;
+        j = r;
+        x = s[i];
+        while(i<j)
+        {
+            while (i<j&&s[j]>x) j--;
+            if (i<j) s[i++] = s[j];
+            while (i<j&&s[i]<x) i++;
+            if (i < j) s[j--] = s[i];
+        }
+        s[i] = x;
+        r95Sort(s, l, i-1);
+        r95Sort(s, i+1, r);
+    }
+
+}
+
+**/
