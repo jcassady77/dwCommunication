bb942f0c3557dd28fb6db599c204f208c4e2d9b6
jcassady77 <jcassady@andrew.cmu.edu>
2021-08-17 14:22:49 -0400

    removed dead code

--- src/dw_listener/src/dw_filterer.cpp
+++ src/dw_listener/src/dw_filterer.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/Imu.h"
 #include "std_msgs/Int64.h"
 #include <sstream>
 #include <string>
@@ -30,6 +31,11 @@ using namespace std;
 using namespace kf;
 
 dw_data nodeInfo;
+
+double IMUXaccel;
+double IMUYaccel;
+double IMUZaccel;
+
 
 
 dw_listener::dwFiltered dw_filtered::buildRosMsgFilter() {
@@ -120,14 +126,13 @@ void dw_filtered::gateFilter2(dw_data msg) {
     XcoordGateFiltered = meanX;
     YcoordGateFiltered = meanY;
 }
-/*
-void IMUcallback(const SomePackage::SomeMessageType::ConstPtr& msg)
+
+void IMUcallback(const sensor_msgs::Imu::ConstPtr& msg)
 {
-    IMUXaccel.data = msg.linear_acceleration[x]
-    IMUYaccel.data = msg.linear_acceleration[y]
-    IMUZaccel.data = msg.linear_acceleration[z]
+    IMUXaccel = msg->linear_acceleration.x;
+    IMUYaccel = msg->linear_acceleration.y;
+    IMUZaccel = msg->linear_acceleration.z;
 }
-*/
 
 void dwCallback(const dw_listener::nodeData::ConstPtr& msg)
 {
@@ -140,29 +145,16 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "dw_filter");
     ros::NodeHandle n;
     ros::Publisher dw_pub = n.advertise<dw_listener::dwFiltered>("dw_filterer", 1000);
+
     ros::Subscriber dw_sub = n.subscribe("/dw_data", 1000, dwCallback);
+    ros::Subscriber imu_sub = n.subscribe("/imu/data", 100, IMUcallback);
+
     double dt = 0.1;
     ros::Rate loop_rate(dt*100);
 
     //ros::Subscriber IMUsub = n.subscribe("/imu/data", 100, IMUcallback);
 
     //Kalman Filter Variables
-    double measurement_mu = 0.0;      // Mean
-    double measurement_sigma = 0.5;   // Standard deviation
-
-    double process_mu = 0.0;
-    double process_sigma = 0.05;
-
-    default_random_engine generator;
-    normal_distribution<double> measurement_noise(measurement_mu, measurement_sigma);
-    normal_distribution<double> process_noise(process_mu, process_sigma);
-    
-    double trueXaccel = 0;
-    double trueYaccel = 0;
-    double trueXvel = 0;
-    double trueYvel = 0;
-    double trueXDpos = 0;
-    double trueYDpos = 0;
 
     // Preparing KF
     mat A = {   {1.0, 0.0, 0.0, 0.0},
@@ -203,32 +195,16 @@ int main(int argc, char **argv)
             filteredData.YcoordGateFiltered = 0;
             filteredData.XcoordKalmanFiltered = 0;
             filteredData.XcoordKalmanFiltered = 0;
-                //message.tagAddress.data = msg.data.substr(msg.data.find("a16")+6, (msg.data.find("R")-msg.data.find("a16")-9));
-                //message.rangeNum.data = stoi(msg.data.substr(msg.data.find("R")+3, (msg.data.find("T")-msg.data.find("R")-5)));
-                //message.timeOfReception.data = stoi(msg.data.substr(msg.data.find("T")+3, (msg.data.find("D")-msg.data.find("T")-5)));
-                //message.distance.data = stoi(msg.data.substr(msg.data.find("D")+3, (msg.data.find("P")-msg.data.find("D")-5)));
-                //message.degrees.data = stoi(msg.data.substr(msg.data.find("P")+3, (msg.data.find("Xcm")-msg.data.find("P")-5)));
-                //message.Xcoord.data = stoi(msg.data.substr(msg.data.find("Xcm")+5, (msg.data.find("Ycm")-msg.data.find("Xcm")-7)));
-                //message.Ycoord.data = stoi(msg.data.substr(msg.data.find("Ycm")+5, (msg.data.find("O")-msg.data.find("Ycm")-7)));
-                //message.clockOffset.data = stoi(msg.data.substr(msg.data.find("O")+3, (msg.data.find("V")-msg.data.find("O")-5)));
-                //message.serviceData.data = stoi(msg.data.substr(msg.data.find("V")+3, (msg.data.find("X\"")-msg.data.find("V")-5)));
-                //message.Xaccel.data = stoi(msg.data.substr(msg.data.find("X\"")+3, (msg.data.find("Y\"")-msg.data.find("X\"")-5)));
-                //message.Yaccel.data = stoi(msg.data.substr(msg.data.find("Y\"")+3, (msg.data.find("Z\"")-msg.data.find("Y\"")-5)));
-                //message.Zaccel.data = stoi(msg.data.substr(msg.data.find("Z\"")+3, (msg.data.find("}}")-msg.data.find("Z\"")-3)));
-                
-                //message.Xaccel.data = IMUXaccel.data;
-                //message.Yaccel.data = IMUYaccel.data;
-                //message.Zaccel.data = IMUZaccel.data;
-                
-            //Filter with gateing 
+
+            //Filter with gating 
            
             filteredData.gateFilter2(nodeInfo);
  
             //Kalman Filter
 
             kf.updateState({
-                    static_cast<double>(0), //X accel
-                    static_cast<double>(0)}, //Y accel
+                    static_cast<double>(IMUXaccel), //X accel
+                    static_cast<double>(IMUYaccel)}, //Y accel
                     {
                     static_cast<double>(filteredData.XcoordGateFiltered), 
                     static_cast<double>(filteredData.YcoordGateFiltered)});
